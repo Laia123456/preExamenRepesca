5743412a42d259dc620d299860464a480c9a36d4
Laia123456 <marinrosaslaia@fpllefia.com>
2026-06-01 15:38:59 +0000

    preexamen4

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-let btnMostrar = document.querySelector("#bntMostrar")
+let bntMostrar = document.querySelector("#bntMostrar")
+let contenido = document.querySelector("#contenido")
+let btnLimpiar = document.querySelector("#btnLimpiar")
+
+//function mostrar (){
+  //  console.log("Botón pulsado")
+//}
+
+//bntMostrar.addEventListener("click", mostrar)
+
+//function textoJS (){
+  //  contenido.innerHTML = "Texto desde JS"
+//}
+
+//bntMostrar.addEventListener("click", textoJS)
+
+function clase (){
+    contenido.classList.add("divContenido")
+}
+
+bntMostrar.addEventListener("click", clase)
+
+function limpiar (){
+    contenido.classList.remove("divContenido")
+}
+
+btnLimpiar.addEventListener("click", limpiar)
