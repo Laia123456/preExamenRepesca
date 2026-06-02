cb0e67dda5481b09b9bf47c22df7e9d5fca4d4bf
Laia123456 <marinrosaslaia@fpllefia.com>
2026-06-02 08:09:31 +0000

    preexamenok

--- index.js
+++ index.js
@@ -1,56 +1,51 @@
-let bntMostrar = document.querySelector("#bntMostrar")
-let contenido = document.querySelector("#contenido")
+let btnMostrar = document.querySelector("#btnMostrar")
+
+//function bntPulsado (){
+  //console.log("Botón pulsado")
+//}
+//btnMostrar.addEventListener("click", bntPulsado)
+
+let divTexto = document.querySelector("#divTexto")
+
+function textoJS (){
+  divTexto.innerHTML = ("Texto desde JS")
+}
+btnMostrar.addEventListener("click", textoJS)
+
+let divNaranja = document.querySelector("#divNaranja")
+let btnNaranja = document.querySelector("#btnNaranja")
+
+function naranja (){
+  divNaranja.classList.add("divContenido")
+}
+btnNaranja.addEventListener("click", naranja)
+
 let btnLimpiar = document.querySelector("#btnLimpiar")
 
-
-//function mostrar (){
-  //  console.log("Botón pulsado")
-//}
-
-//bntMostrar.addEventListener("click", mostrar)
-
-//function textoJS (){
-  //  contenido.innerHTML = "Texto desde JS"
-//}
-
-//bntMostrar.addEventListener("click", textoJS)
-
-function clase (){
-    contenido.classList.add("divContenido")
-}
-
-bntMostrar.addEventListener("click", clase)
-
 function limpiar (){
-    contenido.classList.remove("divContenido")
+  divNaranja.classList.remove("divContenido")
 }
-
 btnLimpiar.addEventListener("click", limpiar)
 
-//
-let selectAlumnos = document.querySelector("#selectAlumnos")
-let btnAlumnos = document.querySelector("#btnAlumnos")
-let alumnos = document.querySelector("#alumnos")
-let inputAlumnos = document.querySelector("#inputAlumnos")
+let inputNombre = document.querySelector("#inputNombre")
+let selectNombres = document.querySelector("#selectNombres")
+let bntAlumno = document.querySelector("#bntAlumno")
+let divAlumnos = document.querySelector("#divAlumnos")
 
-//function nombreAlumnos (){
- // alumnos.innerHTML = (inputAlumnos.value + " " + selectAlumnos.value)
-///}
+//function mostrarAlumnos (){
+  //divAlumnos.innerHTML = (inputNombre.value + " " + selectNombres.value)
+//}
+//bntAlumno.addEventListener("click", mostrarAlumnos)
 
-//btnAlumnos.addEventListener("click", nombreAlumnos)
-//selectAlumnos.addEventListener("change", nombreAlumnos)
+let inputUrl = document.querySelector("#inputUrl")
+let divUrl = document.querySelector("#divUrl")
 
-//
-let inputFoto = document.querySelector("#inputFoto")
-let divInput = document.querySelector("#divInput")
-
-function url (){
-  divInput.innerHTML = `<img src="${inputFoto.value}" alt="">`
+function mostrarUrl (){
+  divUrl.innerHTML = (`<img src="${inputUrl.value}" alt="">`)
 }
+bntAlumno.addEventListener("click", mostrarUrl)
 
-btnAlumnos.addEventListener("click", url)
-
-//
+//ARRAYS 
 let divGuitarras = document.querySelector("#divGuitarras")
 const guitarras = [
   {
@@ -84,13 +79,12 @@ const guitarras = [
 
  ];
 
-function pintarGuitarras (){
+function pintarGuitarras(){
   for (let i = 0; i < guitarras.length; i++){
-    divGuitarras.innerHTML += `<div>${guitarras[i].marca}</div>`
-    divGuitarras.innerHTML += `<div>${guitarras[i].modelo}</div>`
-    divGuitarras.innerHTML += `<div>${guitarras[i].tipo}</div>`
-    divGuitarras.innerHTML += `<div>${guitarras[i].precio}</div>`
+    divGuitarras.innerHTML += `<div>${guitarras[i].marca}</div>`;
+    divGuitarras.innerHTML += `<div>${guitarras[i].modelo}</div>`;
+    divGuitarras.innerHTML += `<div>${guitarras[i].tipo}</div>`;
+    divGuitarras.innerHTML += `<div>${guitarras[i].precio}</div>`;
   }
 }
-
 pintarGuitarras()
