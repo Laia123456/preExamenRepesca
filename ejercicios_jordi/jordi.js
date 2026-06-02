d5825f20db4028dc351f15fb4b6e4f278c38b6c2
Laia123456 <marinrosaslaia@fpllefia.com>
2026-06-02 11:17:52 +0000

    casita

--- ejercicios_jordi/jordi.js
+++ ejercicios_jordi/jordi.js
@@ -1,41 +1,7 @@
-let contenido = document.querySelector("#contenido")
-let bntColor = document.querySelector("#bntColor")
-let bntcolorRojo = document.querySelector("#bntcolorRojo")
-let limpiar = document.querySelector("#limpiar")
+let selectCoches = document.querySelector("#selectCoches")
+let resultadoCoches = document.querySelector("#resultadoCoches")
 
-//cambio clases
-function colorVerde (){
-    contenido.classList.remove("rojo")
-    contenido.classList.add("verde")
+function seleccionarCoches (){
+    resultadoCoches.innerHTML = (`<img src="${selectCoches.value}" alt="">`)
 }
-
-bntColor.addEventListener("click", colorVerde)
-
-function colorRojo (){
-    contenido.classList.remove("verde")
-    contenido.classList.add("rojo")
-}
-
-bntcolorRojo.addEventListener("click", colorRojo)
-
-function limpiarColor (){
-    contenido.classList.remove("verde")
-    contenido.classList.remove("rojo")
-}
-
-limpiar.addEventListener("click", limpiarColor)
-
-//input alerta
-let input = document.querySelector("#input")
-let resultadoInput = document.querySelector("#resultadoInput")
-let bntInput = document.querySelector("#bntInput")
-
-function inputBarça (){
-    if (input.value == "barça") {
-        resultadoInput.innerHTML = `<img src="https://a.espncdn.com/i/teamlogos/soccer/500/83.png" alt="">`;
-    } else {
-        alert("jordi marica");
-    }
-}
-
-bntInput.addEventListener("click", inputBarça)
+selectCoches.addEventListener("change", seleccionarCoches)
